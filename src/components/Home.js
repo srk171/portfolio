a93c6ee3a2e95718c26bb6e1089bd4b0efe475cd
Sivaranjith kumar <sivalatha1710@gmail.com>
2024-08-10 08:50:59 +0530

    resume link uploaded 10/08/24

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ export default function Home(){
             <p className="flex text-2xl lg:text-3xl xl:text-4xl py-5 font-homefont2 whitespace-nowrap">Full-Stack developer </p>
             </h1>
             
-            <p className=" md:hover:scale-150 transition duration-700 lg:ml-5 shadow-inner shadow-black hover:shadow-blue-700 hover:text-blue-700 font-semibold text-lg w-20 rounded-md whitespace-nowrap p-1 "><Link to="https://drive.google.com/file/d/1-RD1e6oo7LOcgeJ_uOixV97FJsS4ixCv/view?usp=drivesdk">HIRE ME  </Link></p>
+            <p className=" md:hover:scale-150 transition duration-700 lg:ml-5 shadow-inner shadow-black hover:shadow-blue-700 hover:text-blue-700 font-semibold text-lg w-20 rounded-md whitespace-nowrap p-1 "><Link to="https://drive.google.com/file/d/11qUemxfaQ2giDrWJjLUHKmQQOu9z4UCY/view?usp=drivesdk">HIRE ME  </Link></p>
            
         </div>
         </div>
